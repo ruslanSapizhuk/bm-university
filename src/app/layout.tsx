654f82a15c454351d01aab0e11f45e8f335733fd
Ruslan Sapizhuk <ruslan.sapizhuk@gmail.com>
2023-11-22 09:23:09 +0200

    feat: add facebook pikcel

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import { Mulish } from 'next/font/google'
+import Script from 'next/script'
+import './globals.css'
+
+const mulish = Mulish({ subsets: ['cyrillic'] })
+
+export const metadata = {
+  title: 'B&M University',
+  description: '',
+}
+
+export default function RootLayout({
+                                     children,
+                                   }: {
+  children: React.ReactNode
+}) {
+    return (
+      <html lang="uk-UA">
+        <body className={mulish.className}>{children}</body>
+        <Script id="facebook-pixel">
+            {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '691462736338304');
+            fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+            <img height="1" width="1" className="hidden"
+                       src="https://www.facebook.com/tr?id=691462736338304&ev=PageView&noscript=1"
+            />
+        </noscript>
+      </html>
+  )
+}
---
+++ src/app/saccess/page.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "@/components/Button";
+import pmLanaImage from "@/assets/PM-Lana.png";
+import Script from 'next/script'
+export default function SaccessPage() {
+
+    return (
+        <section className="md:flex md:justify-between md:gap-20 mb-10 md:mb-20 max-w-5xl m-auto items-end h-full p-10 md:p-20">
+            <div className="flex flex-col md:flex-row gap-10">
+                <div className="basis-1/2">
+                    <Image src={pmLanaImage} alt={"PM Lana"}/>
+                </div>
+                <div className="basis-1/2">
+                    <h2 className="text-xl md:text-2xl uppercase font-bold text-default mb-5 md:mb-10">Оплата пройшла успішно</h2>
+                    <p className="mb-3 md:mb-5">Вітаю! Ти приймаєш участь у моєму  інтенсиві <b>“Product Management”</b></p>
+                    <p className="mb-5">Долучайся внизу за посиланням в чат, де буде проходити навчання</p>
+                    <Link href="https://t.me/+Kxp9AXP5BKg2NGQy">
+                        <Button inverted>Долучитись до чату</Button>
+                    </Link>
+                </div>
+            </div>
+            <Script id={"track"}>
+                {`fbq('track', 'Purchase', {
+                    value: 0.00,
+                    currency: 'USD'
+                });`}
+            </Script>
+        </section>
+    )
+}
